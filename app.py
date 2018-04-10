979394ec063fd2cafb19fbc57dc9e723d5526e49
ms <--->
2018-04-10 13:45:11 +0300

    3rd commit

--- app.py
+++ app.py
@@ -230,4 +230,5 @@ def adduser():
     return render_template('client/adduser.html')
   
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run()
+    "app.run(host='0.0.0.0', port=5000, debug=True)"
