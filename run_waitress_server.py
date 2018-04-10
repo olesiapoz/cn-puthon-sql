380d2d1018d8f6b9fd60c0685700cc7d81647b40
ms <--->
2018-04-10 23:41:24 +0300

    nnn

--- run_waitress_server.py
+++ run_waitress_server.py
@@ -3,8 +3,5 @@ from waitress import serve
 sys.path.insert(0, './app')
 from app import app
 HOST = os.environ.get('SERVER_HOST', 'localhost')
-try:
-    PORT = int(os.environ.get('SERVER_PORT', '5555'))
-except ValueError:
-PORT = 5555
-serve(app,HOST, PORT)  
+PORT = int(os.environ.get('SERVER_PORT', '5555'))
+serve(app, HOST, PORT)
